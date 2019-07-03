acdb1a7ecf58f21c40dc41ee3b9ac5c89b188076
nickyc975 <chenjinlong2016@gmail.com>
2019-07-03 16:17:51 +0800

    Be able to change state.

--- app/src/main/java/com/nickyc975/android/MainActivity.kt
+++ app/src/main/java/com/nickyc975/android/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
@@ -11,11 +13,39 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
-
+    private var logined = false
     private lateinit var toolbar: Toolbar
     private lateinit var drawer: DrawerLayout
     private lateinit var navigation: NavigationView
     private lateinit var menuButton: ImageButton
+
+    private var drawerListener = object: DrawerLayout.DrawerListener {
+        override fun onDrawerStateChanged(newState: Int) {
+
+        }
+
+        override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
+
+        }
+
+        override fun onDrawerClosed(drawerView: View) {
+
+        }
+
+        override fun onDrawerOpened(drawerView: View) {
+            val loginLogout: Button = findViewById(R.id.login_logout)
+            loginLogout.setText(if (logined) R.string.logout else R.string.login)
+            loginLogout.setOnClickListener {
+                if (logined) {
+                    logout()
+                    loginLogout.setText(R.string.login)
+                } else {
+                    login()
+                    loginLogout.setText(R.string.logout)
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +54,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawer = findViewById(R.id.drawer)
         navigation = findViewById(R.id.navigation)
         menuButton = findViewById(R.id.menu_button)
-        menuButton.setOnClickListener { drawer.openDrawer(GravityCompat.START, true) }
+        menuButton.setOnClickListener { drawer.openDrawer(GravityCompat.START) }
+        drawer.addDrawerListener(drawerListener)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -47,5 +78,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         } else {
             super.onBackPressed()
         }
+    }
+
+    private fun login() {
+        logined = true
+    }
+
+    private fun logout() {
+        logined = false
     }
 }
